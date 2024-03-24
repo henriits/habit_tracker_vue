c5742fbbd16725561db1941507c4877a9c459616
henriits <henritsarents@gmail.com>
2024-03-24 12:39:54 +0200

    store , remove habit to local storage

--- habit-tracker/src/views/CreateHabits.vue
+++ habit-tracker/src/views/CreateHabits.vue
@@ -3,19 +3,41 @@
     <AddHabitForm @add="addNewHabit"></AddHabitForm>
     <div>Create habits here!</div>
     <ul>
-        <li v-for="(habit, index) in habits" :key="index">{{ habit }}</li>
+        <li v-for="(habit, index) in habits" :key="index">
+            <span>{{ habit.name }}</span>
+            <button @click="removeHabit(index)">Remove</button>
+        </li>
     </ul>
 </template>
 
-
-
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import AddHabitForm from "../components/AddHabitForm.vue";
 
 const habits = ref([]);
 
+const loadHabitsFromLocalStorage = () => {
+    const savedHabits = localStorage.getItem('habits');
+    if (savedHabits) {
+        habits.value = JSON.parse(savedHabits);
+    }
+};
+
+onMounted(() => {
+    loadHabitsFromLocalStorage();
+});
+
+
+const saveHabitsToLocalStorage = () => {
+    localStorage.setItem('habits', JSON.stringify(habits.value));
+};
 const addNewHabit = (habitName) => {
-    habits.value.push(habitName); // Add the new habit to the list of habits
+    habits.value.push({ name: habitName }); // Add the new habit without checkboxes
+    saveHabitsToLocalStorage();
+};
+
+const removeHabit = (index) => {
+    habits.value.splice(index, 1); // Remove the habit at the specified index
+    saveHabitsToLocalStorage();
 };
 </script>
