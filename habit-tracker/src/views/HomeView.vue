33dca286d94b63863c67b55050db39a0f20a348f
henriits <henritsarents@gmail.com>
2024-03-24 16:56:27 +0200

    Save habit with date to local storage

--- habit-tracker/src/views/HomeView.vue
+++ habit-tracker/src/views/HomeView.vue
@@ -2,14 +2,16 @@
   <div>
     <DateSelector></DateSelector>
     <div>
+      <h3>Select a Date:</h3>
+      <input type="date" v-model="selectedDate">
       <h3>Select a Habit:</h3>
-      <select v-model="selectedHabit" aria-label="selectedHabit">
+      <select v-model="selectedHabit">
         <option disabled value="">Please select a habit</option>
         <option v-for="(habit, index) in habits" :key="index" :value="habit.name">
           {{ habit.name }}
         </option>
       </select>
-      <p>You selected: {{ selectedHabit }}</p>
+      <button @click="attachHabitToSelectedDate">Attach Habit</button>
     </div>
   </div>
 </template>
@@ -19,6 +21,7 @@ import { onMounted, ref } from 'vue';
 import DateSelector from "../components/DatesView.vue";
 
 const habits = ref([]);
+const selectedDate = ref('');
 const selectedHabit = ref('');
 
 onMounted(() => {
@@ -27,4 +30,22 @@ onMounted(() => {
     habits.value = JSON.parse(savedHabits);
   }
 });
+
+function saveHabitToSelectedDate(date, habit) {
+  // Retrieve the existing date-habit mapping from localStorage
+  const dateHabitMapping = JSON.parse(localStorage.getItem('DateHabitMapping')) || {};
+
+  // Add or update the habit for the selected date
+  dateHabitMapping[date] = habit;
+
+  // Save the updated date-habit mapping back to localStorage
+  localStorage.setItem('DateHabitMapping', JSON.stringify(dateHabitMapping));
+}
+function attachHabitToSelectedDate() {
+  if (!selectedDate.value || !selectedHabit.value) return;
+
+  // Assuming you have a function to save the date-habit mapping to localStorage
+  saveHabitToSelectedDate(selectedDate.value, selectedHabit.value);
+}
+
 </script>
