28e86d96f5ff391bff61cdc2478a197ef5840170
henriits <henritsarents@gmail.com>
2024-03-24 18:54:08 +0200

    format date

--- habit-tracker/src/components/DateDetailsView.vue
+++ habit-tracker/src/components/DateDetailsView.vue
@@ -1,17 +1,12 @@
 <template>
     <div>
         <h1>Date Details</h1>
-        <p>{{ selectedDate }}</p>
+        <p>Selected Date: {{ formattedDate }}</p>
         <!-- Add more details here as needed -->
-
     </div>
 </template>
 
-
-
 <script>
-
-
 export default {
     data() {
         return {
@@ -23,6 +18,16 @@ export default {
     },
     watch: {
         '$route.params.date': 'updateSelectedDate'
+    },
+    computed: {
+        formattedDate() {
+            if (!this.selectedDate) return '';
+            const dateObj = new Date(this.selectedDate);
+            const year = dateObj.getFullYear();
+            const month = String(dateObj.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+            const day = String(dateObj.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        }
     },
     methods: {
         updateSelectedDate() {
