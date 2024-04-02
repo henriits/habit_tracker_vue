31fd027095deaa537c84abd8107d7c6e46d8ac34
henriits <henritsarents@gmail.com>
2024-04-02 20:58:36 +0300

    format date

--- habit-tracker/src/components/DatesView.vue
+++ habit-tracker/src/components/DatesView.vue
@@ -49,10 +49,17 @@ const isCurrentDate = (dateString) => {
   );
 };
 
-const handleDateClick = (date) => {
-  router.push({ name: "dateDetails", params: { date } });
+const formatDate = (date) => {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${year}_${month}_${day}`;
 };
 
+const handleDateClick = (date) => {
+  const formattedDate = formatDate(new Date(date));
+  router.push({ name: "dateDetails", params: { date: formattedDate } });
+};
 const scrollToMiddle = () => {
   const container = scrollContainer.value;
   if (container) {
