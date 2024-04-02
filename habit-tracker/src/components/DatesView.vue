da9c9e9c59cd430332d80526603cf2855c051f0e
henriits <henritsarents@gmail.com>
2024-04-02 21:09:32 +0300

    fix: format changed from _ to -

--- habit-tracker/src/components/DatesView.vue
+++ habit-tracker/src/components/DatesView.vue
@@ -53,7 +53,7 @@ const formatDate = (date) => {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
   const day = date.getDate().toString().padStart(2, "0");
-  return `${year}_${month}_${day}`;
+  return `${year}-${month}-${day}`;
 };
 
 const handleDateClick = (date) => {
